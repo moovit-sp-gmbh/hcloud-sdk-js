20f9d5163f8b786554c06bfad649b9cb64457f7f
john dev <joh.dev@gmail.com>
2022-06-02 17:57:33 +0200

    Add IDP backends

--- src/lib/IDP.ts
+++
@@ -1,8 +0,0 @@
-import hcloud, { options } from './hcloud';
-
-export default class IDPService extends hcloud {
-  authenticate = async () => {
-    // const resp = await this.axios.get('https://google.com');
-    return this.opts.api;
-  };
-}

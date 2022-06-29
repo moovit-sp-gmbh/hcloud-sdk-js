46a0e0c961eb0897c377a6b5e54bab423e94ea07
john dev <joh.dev@gmail.com>
2022-06-29 15:07:04 +0200

    Add user-agent to every request

--- src/lib/hcloud.ts
+++ src/lib/hcloud.ts
@@ -2,6 +2,7 @@ import base, { Options } from "./base";
 import IDPService from "./service/IDP";
 import High5Service from "./service/High5";
 import axios from "axios";
+import { version } from "../../package.json";
 
 // tslint:disable-next-line
 export default class hcloud extends base {
@@ -13,11 +14,15 @@ export default class hcloud extends base {
 
         this.IDP = new IDPService(this.opts);
         this.High5 = new High5Service(this.opts);
+
+        axios.defaults.headers.common = {
+            "User-Agent": "hcloud-sdk-js/v" + version,
+        };
     }
 
     setAuthToken(token: string) {
-        axios.defaults.headers.common = {
+        axios.defaults.headers.common = Object.assign(axios.defaults.headers.common, {
             Authorization: token,
-        };
+        });
     }
 }

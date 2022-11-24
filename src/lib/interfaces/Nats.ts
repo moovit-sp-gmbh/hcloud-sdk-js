04c2739e2efc15dbb4bbada3dba9e7f3fcead989
john dev <joh.dev@gmail.com>
2022-11-24 16:08:52 +0100

    Add Nats service

---
+++ src/lib/interfaces/Nats.ts
@@ -0,0 +1,109 @@
+import { Msg } from "nats";
+
+enum NatsSubject {
+    IDP_USER_GENERAL = "hcloud.idp.user.${userId}.general",
+    IDP_USER_MESSAGES = "hcloud.idp.user.${userId}.messages",
+    IDP_USER_SETTINGS_PATS = "hcloud.idp.user.${userId}.settings.pats",
+    IDP_USER_SETTINGS_OAUTH = "hcloud.idp.user.${userId}.settings.oauth",
+
+    IDP_ORGANIZATION_MEMBERS = "hcloud.idp.organization.${organizationId}.,members",
+
+    HIGH5_APPS = "hcloud.high5.organization.${organizationId}.apps",
+    HIGH5_EVENTS = "hcloud.high5.organization.${organizationId}.app.${appId}.events",
+    HIGH5_STREAMS = "hcloud.high5.organization.${organizationId}.app.${appId}.event.${eventId}.streams",
+    HIGH5_SETTINGS_GENERAL = "hcloud.high5.organization.${organizationId}.app.${appId}.settings.general",
+    HIGH5_SETTINGS_WEBHOOKS = "hcloud.high5.organization.${organizationId}.app.${appId}.settings.webhooks",
+}
+
+/**
+ * NatsSubjects creates subject strings to be used with nats.subscribe and nats.publish
+ */
+class NatsSubjects {
+    static IDP = class {
+        static User = class {
+            static GENERAL = (userId: string) => {
+                return NatsSubjects.replace(NatsSubject.IDP_USER_GENERAL, { userId } as NatsSubjectReplacements);
+            };
+            static MESSAGES = (userId: string) => {
+                return NatsSubjects.replace(NatsSubject.IDP_USER_MESSAGES, { userId } as NatsSubjectReplacements);
+            };
+
+            static Settings = class {
+                static PATS = (userId: string) => {
+                    return NatsSubjects.replace(NatsSubject.IDP_USER_SETTINGS_PATS, { userId } as NatsSubjectReplacements);
+                };
+                static OAUTH = (userId: string) => {
+                    return NatsSubjects.replace(NatsSubject.IDP_USER_SETTINGS_OAUTH, { userId } as NatsSubjectReplacements);
+                };
+            };
+        };
+        static Organization = class {
+            static MEMBERS = (organizationId: string) => {
+                return NatsSubjects.replace(NatsSubject.IDP_ORGANIZATION_MEMBERS, { organizationId } as NatsSubjectReplacements);
+            };
+        };
+    };
+
+    static High5 = class {
+        static APPS = (organizationId: string) => {
+            return NatsSubjects.replace(NatsSubject.HIGH5_APPS, { organizationId } as NatsSubjectReplacements);
+        };
+
+        static App = class {
+            static EVENTS = (organizationId: string, appId: string) => {
+                return NatsSubjects.replace(NatsSubject.HIGH5_EVENTS, { organizationId, appId } as NatsSubjectReplacements);
+            };
+
+            static Event = class {
+                static STREAMS = (organizationId: string, appId: string, eventId: string) => {
+                    return NatsSubjects.replace(NatsSubject.HIGH5_STREAMS, { organizationId, appId, eventId } as NatsSubjectReplacements);
+                };
+            };
+
+            static Settings = class {
+                static GENERAL = (organizationId: string, appId: string) => {
+                    return NatsSubjects.replace(NatsSubject.HIGH5_SETTINGS_GENERAL, { organizationId, appId } as NatsSubjectReplacements);
+                };
+                static WEBHOOKS = (organizationId: string, appId: string) => {
+                    return NatsSubjects.replace(NatsSubject.HIGH5_SETTINGS_WEBHOOKS, { organizationId, appId } as NatsSubjectReplacements);
+                };
+            };
+        };
+    };
+
+    private static replace = (subject: string, replacements: NatsSubjectReplacements): string => {
+        subject = subject.replace("${userId}", replacements.userId || "null");
+        subject = subject.replace("${organizationId}", replacements.organizationId || "null");
+        subject = subject.replace("${appId}", replacements.appId || "null");
+        subject = subject.replace("${eventId}", replacements.eventId || "null");
+        subject = subject.replace("${streamId}", replacements.streamId || "null");
+        subject = subject.replace("${designId}", replacements.designId || "null");
+        subject = subject.replace("${nodeId}", replacements.nodeId || "null");
+
+        return subject;
+    };
+}
+
+type NatsSubjectReplacements = {
+    userId?: string;
+    organizationId?: string;
+    appId?: string;
+    eventId?: string;
+    streamId?: string;
+    designId?: string;
+    nodeId?: string;
+};
+
+enum NatsMessageType {
+    ADD = "ADD",
+    UPDATE = "UPDATE",
+    DELETE = "DELETE",
+}
+interface NatsMessage {
+    type: NatsMessageType;
+    object: any;
+}
+
+type NatsCallback = (msg: NatsMessage, rawMsg: Msg) => void;
+
+export { NatsSubjects, NatsMessageType, NatsMessage, NatsCallback, Msg as RawMsg };
---
+++ src/lib/service/nats/Nats.ts
@@ -0,0 +1,146 @@
+import { rejects } from "assert";
+import { NatsConnection, SubscriptionOptions, NatsError, Msg, Subscription, PublishOptions, RequestOptions } from "nats";
+
+import { connect as connectNode } from "nats";
+import { connect as connectWs } from "nats.ws";
+import { NatsMessage, NatsCallback, NatsMessageType } from "../../interfaces/Nats";
+
+interface SubMapEntry {
+    subject: string;
+    sub: Subscription;
+}
+
+/**
+ * Connect and authenticate using email and jwt
+ *
+ * to use websocket from within node, forceWebsocket can be used.
+ * A websocket library is required to be pre-installed and needs to be set as global:
+ * import WebSocket from "ws";
+ * Object.assign(global, { WebSocket: WebSocket });
+ */
+type ConnectParamsJwt = {
+    email: string;
+    jwt: string;
+    servers?: string[];
+    debug?: boolean;
+    forceWebsocket?: boolean;
+};
+/**
+ * Connect and authenticate using username and password
+ * Can only be used with the CUSTOM_AUTH admin!
+ *
+ * to use websocket from within node, forceWebsocket can be used.
+ * A websocket library is required to be pre-installed and needs to be set as global:
+ * import WebSocket from "ws";
+ * Object.assign(global, { WebSocket: WebSocket });
+ */
+type ConnectParamsPassword = {
+    username: string;
+    password: string;
+    servers?: string[];
+    debug?: boolean;
+    forceWebsocket?: boolean;
+};
+
+const isBrowser = typeof window !== "undefined";
+class Nats {
+    private static instance: Nats;
+    private natsConnection: NatsConnection | undefined;
+    private subMap = [] as SubMapEntry[];
+    private connection = isBrowser ? connectWs : connectNode;
+
+    private constructor() {}
+
+    public static getInstance() {
+        if (!this.instance) {
+            this.instance = new Nats();
+        }
+        return this.instance;
+    }
+
+    public async connect(params: ConnectParamsJwt | ConnectParamsPassword): Promise<NatsConnection> {
+        if (params.forceWebsocket) {
+            this.connection = connectWs;
+        }
+
+        if (!params.servers) {
+            // set defaults if servers array is empty
+            if (isBrowser) {
+                // default via traefik exposed servers
+                const s = `${self.location.protocol === "https:" ? "wss" : "ws"}://${self.location.hostname}:${
+                    self.location.port ? self.location.port : self.location.protocol === "https:" ? 443 : 80
+                }`;
+                params.servers = [`${s}/ws/nats/v1/0`, `${s}/ws/nats/v1/1`, `${s}/ws/nats/v1/2`];
+            } else {
+                // default kubernetes internal servers
+                params.servers = ["hcloud-nats-0.hcloud-nats:4222", "hcloud-nats-1.hcloud-nats:4222", "hcloud-nats-2.hcloud-nats:4222"];
+            }
+        }
+        this.natsConnection = await this.connection({
+            debug: params.debug,
+            maxReconnectAttempts: -1,
+            servers: params.servers,
+            user: (params as ConnectParamsJwt).email !== undefined ? (params as ConnectParamsJwt).email : (params as ConnectParamsPassword).username,
+            pass: (params as ConnectParamsJwt).jwt !== undefined ? (params as ConnectParamsJwt).jwt : (params as ConnectParamsPassword).password,
+        });
+        return this.natsConnection;
+    }
+
+    public getConnection = (): NatsConnection | undefined => {
+        return this.natsConnection;
+    };
+
+    public sub(subject: string, callback: NatsCallback, options?: SubscriptionOptions): Promise<Subscription> {
+        let delay = 0;
+
+        if (!this.getConnection() || this.getConnection()?.isClosed()) {
+            delay = 3000;
+        }
+
+        // add delay just in case the connection has not been established yet
+        return new Promise<Subscription>((resolve, reject) => {
+            setTimeout(() => {
+                options = {
+                    ...options,
+                    callback: (err: NatsError | null, msg: Msg) => {
+                        const data = new TextDecoder("utf-8").decode(msg.data);
+                        callback(JSON.parse(data) as NatsMessage, msg);
+                    },
+                };
+                if (!this.getConnection() || !this.getConnection()?.isClosed) {
+                    reject(new Error("connection not established, can not subscribe"));
+                }
+
+                const sub = this.getConnection()?.subscribe(subject, options);
+                if (sub) {
+                    this.subMap.push({ subject, sub: sub } as SubMapEntry);
+                    return resolve(sub);
+                }
+            }, delay);
+        });
+    }
+
+    public unsub(subject: string | Subscription): void {
+        if (subject instanceof String) {
+            this.subMap.forEach((s: SubMapEntry) => {
+                if (s.subject === subject) {
+                    s.sub.unsubscribe();
+                }
+            });
+        } else {
+            (subject as Subscription)?.unsubscribe();
+        }
+    }
+
+    public publish(subject: string, type: NatsMessageType, object: any, options?: PublishOptions): void {
+        const data = new TextEncoder().encode(JSON.stringify({ type: type, object: object } as NatsMessage));
+        this.getConnection()?.publish(subject, data, options);
+    }
+
+    public request(subject: string, type: NatsMessageType, object: any, options?: RequestOptions): void {
+        const data = new TextEncoder().encode(JSON.stringify({ type: type, object: object } as NatsMessage));
+        this.getConnection()?.request(subject, data, options);
+    }
+}
+
+export default Nats;

30d367313300b2eee0fa1a3452b0ca703b92ee07
ArturGrafov <arturgrafov66@googlemail.com>
2023-02-06 11:46:55 +0100

    Add views enum to global interface definition

--- src/lib/interfaces/Global.ts
+++ src/lib/interfaces/Global.ts
@@ -16,3 +16,12 @@ export enum Products {
     fuse = "fuse",
     all = "all",
 }
+
+export enum Views {
+    save_last_view = "save_last_view",
+    account = "/account",
+    high5 = "/high5",
+    auditor = "/auditor",
+    mailer = "/mailer",
+    fuse = "/fuse",
+}

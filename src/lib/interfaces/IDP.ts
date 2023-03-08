07fe780a1210b3672e22be359b10956c59c1ac91
john dev <joh.dev@gmail.com>
2023-03-08 08:25:08 +0100

    Add umami to PublicConfig

--- src/lib/interfaces/IDP.ts
+++ src/lib/interfaces/IDP.ts
@@ -198,13 +198,18 @@ export interface OAuthAppClientSecret {
     modifyDate: number;
 }
 
-interface Region {
+export interface Region {
     name: string;
     id: string;
     server: string;
 }
 
+export interface Umami {
+    id: string;
+}
+
 export interface PublicConfig {
     reCAPTCHA: string;
     regions: Region[];
+    umami: Umami;
 }

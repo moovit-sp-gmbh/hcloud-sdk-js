efde07324980aba063e868739a4ac9c12dfb384f
Hendrik Vosskamp <h.vosskamp@moovit.de>
2023-06-27 12:56:23 +0200

    Add endpoints to remove all users from high5 / fuse space
    * https://app.clickup.com/t/2570196/HCLOUD-1002

--- src/lib/service/fuse/FuseSpaceInternal.ts
+++ src/lib/service/fuse/FuseSpaceInternal.ts
@@ -20,6 +20,17 @@ export class FuseSpaceInternal extends base {
         });
     };
 
+    /**
+     * removeUserFromAllSpacesOfOrganization deletes the user from all spaces of an organization
+     * @param orgName the organizations's name
+     * @param userId the user's id
+     */
+    public removeUserFromAllSpacesOfOrganization = async (orgName: string, userId: string): Promise<void> => {
+        await this.axios.delete<void>(this.getEndpoint(`/v1/org/${orgName}/spaces/user/${userId}`)).catch((err: Error) => {
+            throw err;
+        });
+    };
+
     protected getEndpoint(endpoint: string): string {
         return `${this.options.server}/api/fuse/internal${endpoint}`;
     }

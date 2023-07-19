ada4ca12cab178bc3dadf65382f05f519b772c39
DariusWe <dariusweiberg@gmail.com>
2023-07-19 11:56:11 +0200

    Add SAMLProvider endpoints

--- src/lib/interfaces/IDP.ts
+++ src/lib/interfaces/IDP.ts
@@ -307,3 +307,25 @@ export enum TeamUsersPatchOperation {
     REMOVE = "REMOVE",
     SET = "SET",
 }
+
+export interface FullSAMLProvider {
+    domainName: string;
+    ssoLoginURL: string;
+    ssoLogoutURL: string;
+    certificates: string[];
+    allowUnencryptedAssertion: boolean;
+}
+
+export interface SAMLProvider {
+    ssoLoginURL: string;
+    ssoLogoutURL: string;
+    certificates: string[];
+    allowUnencryptedAssertion: boolean;
+}
+
+export interface SAMLProviderCreateDto {
+    ssoLoginURL: string;
+    ssoLogoutURL: string;
+    certificates: string[];
+    allowUnencryptedAssertion?: boolean;
+}

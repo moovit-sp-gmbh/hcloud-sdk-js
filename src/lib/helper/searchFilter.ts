2b8b5208cc4d6a2432a5e64f24b9cb049e67cafd
DariusWe <dariusweiberg@gmail.com>
2023-07-27 12:31:14 +0200

    Replace getAuditLogs with searchAuditLogs

--- src/lib/helper/searchFilter.ts
+++ src/lib/helper/searchFilter.ts
@@ -10,14 +10,14 @@ import {
 class SearchFilterDTO {
     private type: SearchFilterType;
     private key: string;
-    private value: string | number | boolean | string[];
     private comparator: SearchFilterComparatorString | SearchFilterComparatorNumber | SearchFilterComparatorDate | SearchFilterComparatorBoolean;
+    private value: string | number | boolean | string[];
 
     constructor(searchFilter: SearchFilter) {
         this.type = searchFilter.type;
         this.key = searchFilter.key;
-        this.value = (searchFilter as any).value;
         this.comparator = (searchFilter as any).comparator;
+        this.value = (searchFilter as any).value;
     }
 }
 

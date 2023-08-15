897c910dbfefeb3622f8e0a3b76ca194097a4cba
DariusWe <dariusweiberg@gmail.com>
2023-08-15 13:27:31 +0200

    Fix return type of getCronjobLogs functions and update CronjobLogDto interface

--- src/lib/service/fuse/space/log/FuseCronjobLog.ts
+++ src/lib/service/fuse/space/log/FuseCronjobLog.ts
@@ -1,14 +1,15 @@
 import base from "../../../../base";
+import { CronjobLogDto } from "../../../../interfaces/Fuse";
 
 export class FuseCronjobLog extends base {
     /**
-     * getAllCronjobLogs returns all logs for a cronjob
+     * Returns all logs for a cronjob
      * @param orgName the organizations's name
      * @param spaceName the spaces's name
      * @param cronjobId the cronjob's ID
      * @param limit the maximum results limit (1-100; defaults to 25)
      * @param page the results to skip (page * limit)
-     * @returns CronjobLog array
+     * @returns Array consisting of an array of cronjob logs and the total number of results
      */
     public getAllCronjobLogs = async (
         orgName: string,
@@ -16,17 +17,17 @@ export class FuseCronjobLog extends base {
         cronjobId: string,
         limit?: number,
         page?: number
-    ): Promise<FuseCronjobLog[]> => {
+    ): Promise<[CronjobLogDto[], number]> => {
         limit = limit || 25;
         page = page || 0;
 
         const resp = await this.axios
-            .get<FuseCronjobLog[]>(this.getEndpoint(`/v1/org/${orgName}/spaces/${spaceName}/jobs/${cronjobId}/logs?page=${page}&limit=${limit}`))
+            .get<CronjobLogDto[]>(this.getEndpoint(`/v1/org/${orgName}/spaces/${spaceName}/jobs/${cronjobId}/logs?page=${page}&limit=${limit}`))
             .catch((err: Error) => {
                 throw err;
             });
 
-        return resp.data;
+        return [resp.data, parseInt(String(resp.headers["total"]), 10)];
     };
 
     /**
@@ -36,9 +37,9 @@ export class FuseCronjobLog extends base {
      * @param cronjobId the cronjob's ID
      * @returns CronjobLog
      */
-    public getCronjobLog = async (orgName: string, spaceName: string, cronjobId: string, cronjobLogId: string): Promise<FuseCronjobLog> => {
+    public getCronjobLog = async (orgName: string, spaceName: string, cronjobId: string, cronjobLogId: string): Promise<CronjobLogDto> => {
         const resp = await this.axios
-            .get<FuseCronjobLog>(this.getEndpoint(`/v1/org/${orgName}/spaces/${spaceName}/jobs/${cronjobId}/logs/${cronjobLogId}`))
+            .get<CronjobLogDto>(this.getEndpoint(`/v1/org/${orgName}/spaces/${spaceName}/jobs/${cronjobId}/logs/${cronjobLogId}`))
             .catch((err: Error) => {
                 throw err;
             });

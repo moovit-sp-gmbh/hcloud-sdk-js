2eb91caf95590e145cc9d855e88fa20d518ea361
DariusWe <dariusweiberg@gmail.com>
2023-08-21 10:03:00 +0200

    Fix typo

--- src/lib/interfaces/mailer/index.ts
+++ src/lib/interfaces/mailer/index.ts
@@ -64,7 +64,7 @@ export class IdpNewRegistrationMailjetMailDTO extends MailjetMailDTO {
     }
 }
 
-export class IdpResetPasswprdMailjetMailDTO extends MailjetMailDTO {
+export class IdpResetPasswordMailjetMailDTO extends MailjetMailDTO {
     constructor(recipients: string[], resetPasswordLink: string) {
         super(recipients, MailjetTemplate.IDP_RESET_PASSWORD, {
             HCLOUD_RESET_PASSWORD_LINK: resetPasswordLink,

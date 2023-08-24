d1fc98765ee2d3b8f30285b4d581d17be4e9c536
slev-dev <me@slev.de>
2023-08-24 16:37:21 +0200

    Fix: this is no longer undefined in high5/secrets

--- src/lib/service/high5/space/settings/secret/index.ts
+++ src/lib/service/high5/space/settings/secret/index.ts
@@ -16,13 +16,13 @@ export default class High5Secret extends Base {
      * @param page - (optional) Page number: Skip the first (page * limit) results (defaults to 0)
      * @returns Array of secret keys
      */
-    async getSecrets(orgName: string, spaceName: string, limit = 25, page = 0): Promise<Secret[]> {
+    getSecrets = async (orgName: string, spaceName: string, limit = 25, page = 0): Promise<Secret[]> => {
         const resp = await this.axios.get<Secret[]>(
             this.getEndpoint(`/v1/org/${orgName}/spaces/${spaceName}/settings/secrets?limit=${limit}&page=${page}`)
         );
 
         return resp.data;
-    }
+    };
 
     /**
      * Adds a secret key-value pair to a space.
@@ -33,7 +33,7 @@ export default class High5Secret extends Base {
      * @param encrypted - (optional) Boolean defining if the value should be stored encrypted (defaults to false).
      * @returns The created Secret
      */
-    async addSecret(orgName: string, spaceName: string, key: string, value: string, encrypted = false): Promise<Secret> {
+    addSecret = async (orgName: string, spaceName: string, key: string, value: string, encrypted = false): Promise<Secret> => {
         const resp = await this.axios.post<Secret>(this.getEndpoint(`/v1/org/${orgName}/spaces/${spaceName}/settings/secrets`), {
             key,
             value,
@@ -41,7 +41,7 @@ export default class High5Secret extends Base {
         });
 
         return resp.data;
-    }
+    };
 
     /**
      * Updates the value for an existent secret key-value pair.
@@ -52,14 +52,14 @@ export default class High5Secret extends Base {
      * @param encrypted - (optional) Boolean defining if the value should be stored encrypted (defaults to false).
      * @returns The updated Secret
      */
-    async updateSecret(orgName: string, spaceName: string, key: string, value: string, encrypted = false): Promise<Secret> {
+    updateSecret = async (orgName: string, spaceName: string, key: string, value: string, encrypted = false): Promise<Secret> => {
         const resp = await this.axios.put<Secret>(this.getEndpoint(`/v1/org/${orgName}/spaces/${spaceName}/settings/secrets/${key}`), {
             value,
             encrypted,
         });
 
         return resp.data;
-    }
+    };
 
     /**
      * Deletes a key-value pair of the space
@@ -67,9 +67,9 @@ export default class High5Secret extends Base {
      * @param spaceName - Name of the space
      * @param key - Key of the key-value pair
      */
-    async deleteSecret(orgName: string, spaceName: string, key: string): Promise<void> {
+    deleteSecret = async (orgName: string, spaceName: string, key: string): Promise<void> => {
         await this.axios.delete(this.getEndpoint(`/v1/org/${orgName}/spaces/${spaceName}/settings/secrets/${key}`));
-    }
+    };
 
     protected getEndpoint(endpoint: string): string {
         return `${this.options.server}/api/high5${endpoint}`;

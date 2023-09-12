fa0d857fb1b152e8b550f75d83051eca0a301936
DariusWe <dariusweiberg@gmail.com>
2023-09-12 14:54:50 +0200

    Update interface Secret to hold reduced creator object

--- src/lib/interfaces/high5/space/secret/index.ts
+++ src/lib/interfaces/high5/space/secret/index.ts
@@ -1,7 +1,10 @@
+import { ReducedUser } from "../../../idp";
+
 export interface Secret {
     key: string;
     value?: string;
     encrypted: boolean;
+    creator: ReducedUser;
     createDate: number;
     modifyDate: number;
 }

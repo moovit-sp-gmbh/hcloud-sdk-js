99f26e8185482b65d6073f3c1a6264a13537283c
ArturGrafov <arturgrafov66@googlemail.com>
2024-01-05 16:29:37 +0100

    Update engine/catalog related entities
    - Simplify and align their interfaces as engine has no github release boundary anymore
    - Update engine patch version for space endpoint and add the ones for catalogs
    - Update engine/catalog s3 download endpoints

---
+++ src/lib/service/high5/space/wave/index.ts
@@ -0,0 +1,67 @@
+import { AxiosInstance } from "axios";
+import Base, { Options } from "../../../../Base";
+import { SpacePatchWaveCatalog, SpacePatchWaveEngine, WaveCatalog, WaveEngine } from "../../../../interfaces/high5";
+
+export default class High5Wave extends Base {
+    constructor(options: Options, axios: AxiosInstance) {
+        super(options, axios);
+    }
+
+    /**
+     * Patch wave engine version for a space
+     * @param orgName - Name of the organization
+     * @param spaceName - Name of the space
+     * @param spacePatchWaveEngine - Patch wave engine object holding url and version
+     * @returns WaveEngine object
+     */
+    public patchSpaceWaveEngine = async (orgName: string, spaceName: string, spacePatchWaveEngine: SpacePatchWaveEngine): Promise<WaveEngine> => {
+        const resp = await this.axios.patch<WaveEngine>(this.getEndpoint(`/v1/org/${orgName}/spaces/${spaceName}/wave/engine`), spacePatchWaveEngine);
+
+        return resp.data;
+    };
+
+    /**
+     * Get wave engine version currently active for the space
+     * @param orgName - Name of the organization
+     * @param spaceName - Name of the space
+     * @returns WaveEngine object
+     */
+    public getSpaceWaveEngine = async (orgName: string, spaceName: string): Promise<WaveEngine> => {
+        const resp = await this.axios.get<WaveEngine>(this.getEndpoint(`/v1/org/${orgName}/spaces/${spaceName}/wave/engine`));
+
+        return resp.data;
+    };
+
+    /**
+     * Adds a new wave catalog version for a space or updates existing
+     * @param orgName - Name of the organization
+     * @param spaceName - Name of the space
+     * @param spacePatchWaveCatalog - Patch wave catalog object holding url and version
+     * @returns WaveCatalog object
+     */
+    public addUpdateSpaceWaveCatalog = async (orgName: string, spaceName: string, spacePatchCatalog: SpacePatchWaveCatalog): Promise<WaveCatalog> => {
+        const resp = await this.axios.post<WaveCatalog>(this.getEndpoint(`/v1/org/${orgName}/spaces/${spaceName}/wave/catalog`), spacePatchCatalog);
+
+        return resp.data;
+    };
+
+    /**
+     * Get wave catalogs currently active for the space
+     * @param orgName - Name of the organization
+     * @param spaceName - Name of the space
+     * @returns WaveCatalog array
+     */
+    public getSpaceWaveCatalogs = async (orgName: string, spaceName: string): Promise<WaveCatalog[]> => {
+        const resp = await this.axios.get<WaveCatalog[]>(this.getEndpoint(`/v1/org/${orgName}/spaces/${spaceName}/wave/catalog`));
+
+        return resp.data;
+    };
+
+    public deleteSpaceWaveCatalog = async (orgName: string, spaceName: string, catalogId: string): Promise<void> => {
+        await this.axios.delete(this.getEndpoint(`/v1/org/${orgName}/spaces/${spaceName}/wave/catalog/${catalogId}`));
+    };
+
+    protected getEndpoint(endpoint: string): string {
+        return `${this.options.server}/api/high5${endpoint}`;
+    }
+}

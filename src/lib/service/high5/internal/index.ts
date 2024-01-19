65abd10756bee4b5408ce6d8128fb855be87caa2
Jorge Brown <j.brown@moovit-sp.com>
2024-01-19 14:11:25 +0100

    Update High5SpaceInternal to be under High5Internal
    - Now instead of hcl.High5.spaceInternal use hcl.High5.internal.space.

--- src/lib/service/high5/internal/index.ts
+++ src/lib/service/high5/internal/index.ts
@@ -1,7 +1,15 @@
 import { AxiosInstance } from "axios";
 import Base, { Options } from "../../../Base";
+import { High5SpaceInternal } from "./space";
 
 export class High5Internal extends Base {
+    public space: High5SpaceInternal;
+
+    constructor(options: Options, axios: AxiosInstance) {
+        super(options, axios);
+        this.space = new High5SpaceInternal(options, axios);
+    }
+
     async deleteUsers(userIds: string[]): Promise<void> {
         await this.axios.delete(this.getEndpoint("/v1/users"), { data: { userIds } });
     }

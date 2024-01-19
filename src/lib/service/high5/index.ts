befd63e75b046a0d910fba222852399bf0659f81
Jorge Brown <j.brown@moovit-sp.com>
2024-01-19 14:11:25 +0100

    Update High5SpaceInternal to be under High5Internal
    - Now instead of hcl.High5.spaceInternal use hcl.High5.internal.space.

--- src/lib/service/high5/index.ts
+++ src/lib/service/high5/index.ts
@@ -2,7 +2,6 @@ import { AxiosInstance } from "axios";
 import Base, { Options } from "../../Base";
 import { Version } from "../../interfaces/global";
 import { High5Space } from "./space";
-import { High5SpaceInternal } from "./internal/space";
 import { High5Wave } from "./wave";
 import { High5JoinToken } from "./joinToken";
 import { High5OrganizationExecute } from "./execution";
@@ -10,7 +9,6 @@ import { High5Internal } from "./internal";
 
 export default class High5 extends Base {
     public space: High5Space;
-    public spaceInternal: High5SpaceInternal;
     public internal: High5Internal;
     public wave: High5Wave;
     public joinToken: High5JoinToken;
@@ -20,7 +18,6 @@ export default class High5 extends Base {
         super(options, axios);
 
         this.space = new High5Space(this.options, this.axios);
-        this.spaceInternal = new High5SpaceInternal(this.options, this.axios);
         this.internal = new High5Internal(this.options, this.axios);
         this.wave = new High5Wave(this.options, this.axios);
         this.joinToken = new High5JoinToken(this.options, this.axios);

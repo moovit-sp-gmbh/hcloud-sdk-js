2cbe929f533f99cd0736b0eff770eb5c3fe5b532
Jorge Brown <j.brown@moovit-sp.com>
2024-01-19 14:10:33 +0100

    Add internal method to Fuse to delete users

--- src/lib/service/fuse/internal/index.ts
+++ src/lib/service/fuse/internal/index.ts
@@ -1,11 +1,20 @@
 import { AxiosInstance } from "axios";
-import { Options } from "../../../Base";
+import Base, { Options } from "../../../Base";
 import { FuseSpaceInternal } from "./space";
 
-export class FuseInternal {
+export class FuseInternal extends Base {
     public space: FuseSpaceInternal;
 
     constructor(options: Options, axios: AxiosInstance) {
+        super(options, axios);
         this.space = new FuseSpaceInternal(options, axios);
+    }
+
+    async deleteUsers(userIds: string[]): Promise<void> {
+        await this.axios.delete(this.getEndpoint("/v1/users"), { data: { userIds } });
+    }
+
+    protected getEndpoint(endpoint: string): string {
+        return `${this.options.server}/api/fuse/internal${endpoint}`;
     }
 }

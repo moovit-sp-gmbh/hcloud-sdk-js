90efc918bae3afa9a03568ec6c81d794955c4920
john-dev <me@slev.de>
2024-01-30 11:01:57 +0100

    Chg: omit bouncer features dates and creator

--- src/lib/interfaces/bouncer/features/index.ts
+++ src/lib/interfaces/bouncer/features/index.ts
@@ -2,9 +2,6 @@ export interface HcloudFeature {
     feature: {
         name: string;
         enabled: boolean;
-        creator: string;
-        createDate: number;
-        modifyDate: number;
     };
     id?: string;
     regions: string[];

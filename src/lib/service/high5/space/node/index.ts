89aeb342e7b1a3fdf265f156d02596c4b2359cbc
Jorge Brown <j.brown@moovit-sp.com>
2024-02-07 17:39:32 +0100

    Move nodes from the stream level to space level

--- src/lib/service/high5/space/node/index.ts
+++ src/lib/service/high5/space/node/index.ts
@@ -1,30 +1,19 @@
-import Base from "../../../../../../Base";
-import { Node, NodeCategory } from "../../../../../../interfaces/high5/space/event/stream/node";
+import Base from "../../../../Base";
+import { Node, NodeCategory } from "../../../../interfaces/high5/space/event/stream/node";
 
-export class High5Node extends Base {
+export default class High5Node extends Base {
     /**
      * Retrieves all Nodes for the specified stream.
      * @param orgName Name of the organization
      * @param spaceName Name of the space
-     * @param eventName Name of the event
-     * @param streamId ID of the stream
      * @param limit (optional) Max number of results (1-100; defaults to 25)
      * @param page (optional) Page number: Skip the first (page * limit) results (defaults to 0)
      * @returns Array containg the Nodes
      */
-    public async getAllNodes(
-        orgName: string,
-        spaceName: string,
-        eventName: string,
-        streamId: string,
-        limit?: number,
-        page?: number
-    ): Promise<Node[]> {
+    public async getAllNodes(orgName: string, spaceName: string, limit?: number, page?: number): Promise<Node[]> {
         limit = limit || 25;
         page = page || 0;
-        const resp = await this.axios.get<Node[]>(
-            this.getEndpoint(`/v1/org/${orgName}/spaces/${spaceName}/events/${eventName}/streams/${streamId}/nodes?page=${page}&limit=${limit}`)
-        );
+        const resp = await this.axios.get<Node[]>(this.getEndpoint(`/v1/org/${orgName}/spaces/${spaceName}/nodes?page=${page}&limit=${limit}`));
 
         return resp.data;
     }
@@ -33,15 +22,11 @@ export class High5Node extends Base {
      * Retrieves a Node by its ID.
      * @param orgName Name of the organization
      * @param spaceName Name of the space
-     * @param eventName Name of the event
-     * @param streamId ID of the stream
      * @param nodeId Id of the Node
      * @returns The requested Node
      */
-    public async getNode(orgName: string, spaceName: string, eventName: string, streamId: string, nodeId: string): Promise<Node> {
-        const resp = await this.axios.get<Node>(
-            this.getEndpoint(`/v1/org/${orgName}/spaces/${spaceName}/events/${eventName}/streams/${streamId}/nodes/${nodeId}`)
-        );
+    public async getNode(orgName: string, spaceName: string, nodeId: string): Promise<Node> {
+        const resp = await this.axios.get<Node>(this.getEndpoint(`/v1/org/${orgName}/spaces/${spaceName}/nodes/${nodeId}`));
 
         return resp.data;
     }
@@ -50,8 +35,6 @@ export class High5Node extends Base {
      * Adds a new Node to a stream.
      * @param orgName Name of the organization
      * @param spaceName Name of the space
-     * @param eventName Name of the event
-     * @param streamId ID of the stream
      * @param nodeCategory Category of the node
      * @param specification Specification of the node as a base64 string
      * @param typescript Typescript code as a base64 string
@@ -60,20 +43,15 @@ export class High5Node extends Base {
     public async createNode(
         orgName: string,
         spaceName: string,
-        eventName: string,
-        streamId: string,
         nodeCategory: NodeCategory,
         specification: string,
         typescript: string
     ): Promise<Node> {
-        const resp = await this.axios.post<Node>(
-            this.getEndpoint(`/v1/org/${orgName}/spaces/${spaceName}/events/${eventName}/streams/${streamId}/nodes`),
-            {
-                category: nodeCategory,
-                specification: specification,
-                typescript: typescript,
-            }
-        );
+        const resp = await this.axios.post<Node>(this.getEndpoint(`/v1/org/${orgName}/spaces/${spaceName}/nodes`), {
+            category: nodeCategory,
+            specification: specification,
+            typescript: typescript,
+        });
 
         return resp.data;
     }
@@ -82,22 +60,16 @@ export class High5Node extends Base {
      * Deletes a Node by its ID
      * @param orgName Name of the organization
      * @param spaceName Name of the space
-     * @param eventName Name of the event
-     * @param streamId ID of the stream
      * @param nodeId Id of the node
      */
-    public async deleteNode(orgName: string, spaceName: string, eventName: string, streamId: string, nodeId: string): Promise<void> {
-        await this.axios.delete<void>(
-            this.getEndpoint(`/v1/org/${orgName}/spaces/${spaceName}/events/${eventName}/streams/${streamId}/nodes/${nodeId}`)
-        );
+    public async deleteNode(orgName: string, spaceName: string, nodeId: string): Promise<void> {
+        await this.axios.delete<void>(this.getEndpoint(`/v1/org/${orgName}/spaces/${spaceName}/nodes/${nodeId}`));
     }
 
     /**
      * Patches the Typescript code and specification of the Node.
      * @param orgName Name of the organization
      * @param spaceName Name of the space
-     * @param eventName Name of the event
-     * @param streamId ID of the stream
      * @param nodeId ID of the node
      * @param regenerateSecret Boolean to specify if a new secret shall be created
      * @param specification (optional) Specification of the node as a base64 string
@@ -107,8 +79,6 @@ export class High5Node extends Base {
     public async patchNode(
         orgName: string,
         spaceName: string,
-        eventName: string,
-        streamId: string,
         nodeId: string,
         regenerateSecret: boolean,
         specification?: string,
@@ -126,9 +96,7 @@ export class High5Node extends Base {
         }
 
         const resp = await this.axios.patch<Node>(
-            this.getEndpoint(
-                `/v1/org/${orgName}/spaces/${spaceName}/events/${eventName}/streams/${streamId}/nodes/${nodeId}?regenerateSecret=${regenerateSecret}`
-            ),
+            this.getEndpoint(`/v1/org/${orgName}/spaces/${spaceName}/nodes/${nodeId}?regenerateSecret=${regenerateSecret}`),
             patchNode
         );
 
@@ -139,23 +107,12 @@ export class High5Node extends Base {
      * Retrieves the content (raw javascript code) of a Node.
      * @param orgName Name of the organization
      * @param spaceName Name of the space
-     * @param eventName Name of the event
-     * @param streamId ID of the stream
      * @param nodeId ID of the node
      * @param secret Secret of the Node (a unique sha512 hash)
      * @rerurns Raw javascript representation of the Node's content
      */
-    public async getNodeContent(
-        orgName: string,
-        spaceName: string,
-        eventName: string,
-        streamId: string,
-        nodeId: string,
-        secret: string
-    ): Promise<string> {
-        const resp = await this.axios.get<string>(
-            this.getEndpoint(`/v1/org/${orgName}/spaces/${spaceName}/events/${eventName}/streams/${streamId}/nodes/${nodeId}/content/${secret}`)
-        );
+    public async getNodeContent(orgName: string, spaceName: string, nodeId: string, secret: string): Promise<string> {
+        const resp = await this.axios.get<string>(this.getEndpoint(`/v1/org/${orgName}/spaces/${spaceName}/nodes/${nodeId}/content/${secret}`));
 
         return resp.data;
     }

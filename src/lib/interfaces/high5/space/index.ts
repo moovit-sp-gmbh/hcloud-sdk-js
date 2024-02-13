0d405a3b794f2c6feb7810ae3c29f33b77e626c6
DariusWe <dariusweiberg@gmail.com>
2024-02-13 13:20:11 +0100

    refactor: remove centralized SpacePermission and SpaceEntityPermission interface and create separate ones for Fuse and High5

--- src/lib/interfaces/high5/space/index.ts
+++ src/lib/interfaces/high5/space/index.ts
@@ -1,5 +1,22 @@
-import { Space } from "../../global";
+import { Space, SpaceEntity } from "../../global";
+
+type ExtendWith<T1, T2> = T1 & T2;
 
 export interface High5Space extends Space {
+    permissions: ExtendWith<High5SpaceEntityPermission, { name: string }>[];
     waveEngine: string;
+}
+export interface High5SpaceEntityPermission {
+    entityId: string;
+    type: SpaceEntity;
+    permission: High5SpacePermission;
+}
+
+export enum High5SpacePermission {
+    NONE = "NONE",
+    READ = "READ",
+    EXECUTE = "EXECUTE",
+    WRITE = "WRITE",
+    MANAGE = "MANAGE",
+    OWNER = "OWNER",
 }

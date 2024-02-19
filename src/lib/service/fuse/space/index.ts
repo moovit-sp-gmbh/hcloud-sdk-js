87db01437e47ca539e691ab46462113feaf705b2
ArturGrafov <arturgrafov66@googlemail.com>
2024-02-19 15:43:15 +0100

    (fuse) Add search space permissions endpoint and update the patch permissions endpoints

--- src/lib/service/fuse/space/index.ts
+++ src/lib/service/fuse/space/index.ts
@@ -1,6 +1,6 @@
 import { AxiosInstance } from "axios";
 import Base, { Options } from "../../../Base";
-import { PaginatedResponse, SearchFilter, SearchParams } from "../../../interfaces/global";
+import { PaginatedResponse, SearchFilter, SearchParams, SpaceEntityPermission, SpacePermissionResponse } from "../../../interfaces/global";
 import { SearchFilterDTO } from "../../../helper/searchFilter";
 import { createPaginatedResponse } from "../../../helper/paginatedResponseHelper";
 import { FuseCronjob } from "./cronjob";
@@ -55,8 +55,13 @@ export class FuseSpace extends Base {
      * @param permission New permission
      * @returns The Fuse space with the updated permissions
      */
-    public patchUserSpacePermission = async (orgName: string, spaceName: string, userId: string, permission: SpacePermission): Promise<FuseSpace> => {
-        const resp = await this.axios.patch<FuseSpace>(this.getEndpoint(`/v1/org/${orgName}/spaces/${spaceName}/permission/user`), {
+    public patchUserSpacePermission = async (
+        orgName: string,
+        spaceName: string,
+        userId: string,
+        permission: SpacePermission
+    ): Promise<SpaceEntityPermission> => {
+        const resp = await this.axios.patch<SpaceEntityPermission>(this.getEndpoint(`/v1/org/${orgName}/spaces/${spaceName}/permission/user`), {
             userId,
             permission,
         });
@@ -77,8 +82,8 @@ export class FuseSpace extends Base {
         spaceName: string,
         teamName: string,
         permission: SpacePermission
-    ): Promise<IFuseSpace> => {
-        const resp = await this.axios.patch<IFuseSpace>(this.getEndpoint(`/v1/org/${orgName}/spaces/${spaceName}/permission/team`), {
+    ): Promise<SpaceEntityPermission> => {
+        const resp = await this.axios.patch<SpaceEntityPermission>(this.getEndpoint(`/v1/org/${orgName}/spaces/${spaceName}/permission/team`), {
             teamName,
             permission,
         });
@@ -112,6 +117,38 @@ export class FuseSpace extends Base {
         });
 
         return createPaginatedResponse(resp) as PaginatedResponse<IFuseSpace>;
+    };
+
+    /**
+     * Retrieves Permissions of a space inside an Organization matching the search filter(s). Will return all Permissions if no search filter is provided
+     * and user has enough rights to see them, an empty array otherwise.
+     * @param orgName Name of the organization
+     * @param spaceName Name of the space
+     * @param filters (optional) Array of search filters
+     * @param sorting (optional) Sorting object
+     * @param limit (optional) Max number of results (1-100; defaults to 25)
+     * @param page (optional) Page number: Skip the first (page * limit) results (defaults to 0)
+     * @returns Object containing an array of High5 Space Permissions and the total number of results found in the database (independent of limit and page)
+     */
+    public searchSpacePermissions = async ({
+        orgName,
+        spaceName,
+        filters,
+        sorting,
+        limit = 25,
+        page = 0,
+    }: SearchParams & { orgName: string; spaceName: string }): Promise<PaginatedResponse<SpacePermissionResponse>> => {
+        const filtersDTO = filters?.map((f: SearchFilter) => new SearchFilterDTO(f));
+
+        const resp = await this.axios.post<SpacePermissionResponse[]>(
+            this.getEndpoint(`/v1/org/${orgName}/spaces/${spaceName}/permissions/search?page=${page}&limit=${limit}`),
+            {
+                filters: filtersDTO,
+                sorting: sorting,
+            }
+        );
+
+        return createPaginatedResponse(resp) as PaginatedResponse<SpacePermissionResponse>;
     };
 
     /**

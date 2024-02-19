e2ba9b1cfb3285d0019e27e2e81b224f0e2e2615
DariusWe <dariusweiberg@gmail.com>
2024-02-19 12:21:42 +0100

    chore: add pre-push hook to prevent commits of type 'tmp' to be pushed to remote

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,4 +1,9 @@
 /*
+ * This config is used locally by the commit-msg hook to validate commit messages.
+ * It implements the conventional commit specification, but also allows the additional commit
+ * type called "tmp", which can be used to do temporary commits that are intended to be 
+ * renamed or squashed later on. This commit message type cannot be pushed to remote.
+ *
  * docs: https://commitlint.js.org/#/reference-configuration
  */
 

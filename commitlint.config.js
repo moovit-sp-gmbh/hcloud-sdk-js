fbc4d9c1d04fd70aa243f4e406d2dcf6aa83a0b5
DariusWe <dariusweiberg@gmail.com>
2024-02-22 15:16:06 +0100

    chore: prevent commit-msg hook from running in GitHub workflow

--- commitlint.config.js
+++ commitlint.config.js
@@ -28,11 +28,6 @@ module.exports = {
         "tmp",
       ],
     ],
-    /**
-     * There is (at least) one commit message in the staging history that exceeds the default value of 100. After first release
-     * on staging branch, this line can be removed, to further only allow 100 chars
-     */
-    "body-max-line-length": [2, "always", 250]
   },
   helpUrl:
     "https://github.com/conventional-changelog/commitlint/#what-is-commitlint",

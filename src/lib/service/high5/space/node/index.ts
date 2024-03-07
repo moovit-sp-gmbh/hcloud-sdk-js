1c92de03b11291f12b7547f32edde22a6e46899e
Oleksandr Hryshchenko <grisov.github@mailnull.com>
2024-03-07 17:22:40 +0100

    refactor: update the method to get node content by secret

--- src/lib/service/high5/space/node/index.ts
+++ src/lib/service/high5/space/node/index.ts
@@ -105,14 +105,11 @@ export default class High5Node extends Base {
 
     /**
      * Retrieves the content (raw javascript code) of a Node.
-     * @param orgName Name of the organization
-     * @param spaceName Name of the space
-     * @param nodeId ID of the node
      * @param secret Secret of the Node (a unique sha512 hash)
-     * @rerurns Raw javascript representation of the Node's content
+     * @returns Raw javascript representation of the Node's content
      */
-    public async getNodeContent(orgName: string, spaceName: string, nodeId: string, secret: string): Promise<string> {
-        const resp = await this.axios.get<string>(this.getEndpoint(`/v1/org/${orgName}/spaces/${spaceName}/nodes/${nodeId}/content/${secret}`));
+    public async getNodeContent(secret: string): Promise<string> {
+        const resp = await this.axios.get<string>(this.getEndpoint(`/v1/org/nodes/content/${secret}`));
 
         return resp.data;
     }

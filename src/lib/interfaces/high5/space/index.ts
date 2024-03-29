052b222428af187c04e932a95a2dc0922701f512
DariusWe <dariusweiberg@gmail.com>
2024-03-29 13:42:09 +0100

    feat!: rename "permissionOfUser" to "highestPermissionOfUser" and make it mandatory

--- src/lib/interfaces/high5/space/index.ts
+++ src/lib/interfaces/high5/space/index.ts
@@ -2,7 +2,7 @@ import { ReducedSpace, Space, SpaceEntity } from "../../global";
 import { ReducedOrganization } from "../../idp";
 
 export interface High5Space extends Space {
-    permissionOfUser?: High5SpacePermission;
+    highestPermissionOfUser: High5SpacePermission;
     waveEngine: string;
 }
 export interface High5SpaceEntityPermission {

cd69aae29aef54e76ec115f50969303fc6b073be
Oleksandr Hryshchenko <grisov.github@mailnull.com>
2024-04-22 12:06:41 +0200

    refactor: remove method to search execution statuses in space

--- src/lib/service/high5/space/execution/status/index.ts
+++ src/lib/service/high5/space/execution/status/index.ts
@@ -10,36 +10,6 @@ export class High5SpaceExecutionStates extends Base {
     }
 
     /**
-     * Retrieves all stream execution logs for a given execution inside a space.
-     * @param orgName Name of the Organization
-     * @param spaceName Name of the Space
-     * @param filters (optional) Array of search filters
-     * @param sorting (optional) Sorting object
-     * @param limit (optional) Max number of results (1-100; defaults to 25)
-     * @param page (optional) Page number: Skip the first (page * limit) results (defaults to 0)
-     * @returns Object containing an array of Stream execution logs as well as the total number of results
-     */
-    public searchExecutionStates = async ({
-        orgName,
-        spaceName,
-        filters,
-        sorting,
-        limit = 25,
-        page = 0,
-    }: SearchParams & { orgName: string; spaceName: string }): Promise<PaginatedResponse<High5ExecutionStatus>> => {
-        const filtersDTO = filters?.map((f: SearchFilter) => new SearchFilterDTO(f));
-        const resp = await this.axios.post<High5ExecutionStatus[]>(
-            this.getEndpoint(`/v1/org/${orgName}/spaces/${spaceName}/execution/status/search?page=${page}&limit=${limit}`),
-            {
-                filters: filtersDTO,
-                sorting: sorting,
-            }
-        );
-
-        return createPaginatedResponse(resp) as PaginatedResponse<High5ExecutionStatus>;
-    };
-
-    /**
      * Retrieves all stream execution logs for a given stream.
      * @param orgName Name of the Organization
      * @param spaceName Name of the Space

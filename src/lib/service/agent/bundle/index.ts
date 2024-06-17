216835edb3074bf4ae8c9c1ec247f145bee725d6
Jorge Brown <j.brown@moovit-sp.com>
2024-06-17 17:06:07 +0200

    feat!: rename bundle dev-release to dev

--- src/lib/service/agent/bundle/index.ts
+++ src/lib/service/agent/bundle/index.ts
@@ -14,22 +14,37 @@ export class AgentBundle extends Base {
     async getVersions(): Promise<AgentVersions> {
         const resp = await this.axios.get<AgentVersions>(this.getEndpoint("/bundles/index.json"), { headers: disableCacheHeaders });
 
+        const registry = resp.data;
+
+        for (const v in registry.versions) {
+            setDev(registry.versions[v]);
+        }
+
         return resp.data;
     }
 
     async getLatestVersion(): Promise<Version> {
         const resp = await this.getVersions();
 
-        return resp.versions[resp.latest];
+        return setDev(resp.versions[resp.latest]);
     }
 
     async getVersion(version: string): Promise<Version | void> {
         const resp = await this.getVersions();
 
-        return resp.versions[version];
+        return setDev(resp.versions[version]);
     }
 
     protected getEndpoint(endpoint: string): string {
         return this.sourceServer + endpoint;
     }
+}
+
+function setDev(v: any): any {
+    // We check false because this way if there is a problem
+    // and the registry gets updated without a dev flag it will
+    // be undefined and therefore considered dev since false !== undefined
+    v.dev = v["dev-release"] !== false;
+    delete v["dev-release"];
+    return v;
 }

30c08724fea68d3555991d05a314a42ade332a30
YoSev <severinsiebertz@googlemail.com>
2024-07-11 09:00:43 +0200

    feat: add frontend config interfaces

---
+++ src/lib/interfaces/frontend/config/index.ts
@@ -0,0 +1,35 @@
+export enum AnnouncementType {
+    INFO = "info",
+    SUCCESS = "success",
+    WARNING = "warning",
+    ERROR = "error",
+}
+
+export interface OIDCProvider {
+    identifier: "github" | "microsoft" | "google" | "discord" | "apple";
+    icon: string;
+    enabled: boolean;
+}
+
+export interface Announcement {
+    type: AnnouncementType;
+    message: string;
+    showWarning?: boolean;
+}
+
+export interface Region {
+    name: string;
+    id: string;
+    server: string;
+    icon?: string;
+    enabled?: boolean;
+    dev?: boolean;
+    umami?: { id: string };
+    announcement?: Announcement;
+}
+
+export interface Config {
+    reCAPTCHA: string;
+    regions: Region[];
+    oidc: OIDCProvider[];
+}

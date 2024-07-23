1aed6e7fb6bca328831db09f8d36654b233995ef
ArturGrafov <arturgrafov66@googlemail.com>
2024-07-23 13:22:37 +0200

    feat: reflect the allow dev bundle switch of agent

--- src/lib/service/agent/index.ts
+++ src/lib/service/agent/index.ts
@@ -1,18 +1,21 @@
 import { AxiosInstance } from "axios";
 import Base, { Options } from "../../Base";
 import { AgentBundle } from "./bundle";
+import { DevBundle } from "./bundle/dev";
 import { AgentContext } from "./context";
 import { AgentInstaller } from "./installer";
 
 export default class Agent extends Base {
     public context: AgentContext;
     public bundle: AgentBundle;
+    public devBundle: DevBundle;
     public installer: AgentInstaller;
 
     constructor(options: Options, axios: AxiosInstance) {
         super(options, axios);
         this.context = new AgentContext(this.options, this.axios);
         this.bundle = new AgentBundle(this.options, this.axios);
+        this.devBundle = new DevBundle(this.options, this.axios);
         this.installer = new AgentInstaller(this.options, this.axios);
     }
 

840981aadbcbcc9b7ef4184489859b65ed9c4971
YoSev <severinsiebertz@googlemail.com>
2024-07-30 15:14:14 +0200

    feat: remove bouncer frontend config

--- src/lib/interfaces/frontend/bouncer/index.ts
+++
@@ -1,9 +0,0 @@
-export interface Feature {
-    name: string;
-    enabled: boolean;
-    type: "feature" | "ab";
-    percentage?: number;
-    users: string[];
-    regions: string[];
-    organizations: string[];
-}

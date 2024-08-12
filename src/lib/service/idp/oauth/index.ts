033f1ffb6f566ff8d1fb749ab1842c189ae9e3e1
Jorge Brown <j.brown@moovit-sp.com>
2024-08-12 14:58:02 +0200

    feat: add preflight OAuth method

--- src/lib/service/idp/oauth/index.ts
+++ src/lib/service/idp/oauth/index.ts
@@ -1,6 +1,7 @@
 import { AxiosInstance } from "axios";
 import Base, { Options } from "../../../Base";
 import { OAuthToken, OAuthTokenRequest } from "../../../interfaces/idp/oauth";
+import { OAuthApp } from "../../../interfaces/idp/organization/settings/oauthApp";
 
 export class IdpOAuth extends Base {
     constructor(options: Options, axios: AxiosInstance) {
@@ -28,6 +29,42 @@ export class IdpOAuth extends Base {
         if (doNotRedirect) {
             return response.data.redirectUrl;
         }
+        return response.headers.location;
+    };
+
+    /**
+     * Attempt to get information about a certain OAuth app. If the user has already consented to all the scopes required
+     * this request will instead return the redirect URL.
+     *
+     * If the user has not consented the return of this function will be an OAuthApp. To consent the
+     * createScopesAndGetAuthorizationCodeInsideRedirectUrl method should be used.
+     * @see IdpOAuth.createScopesAndGetAuthorizationCodeInsideRedirectUrl
+     *
+     * @param queryString {string} - Query string containing the OAuth parameters: response_type, client_id, scope, state
+     * @param doNotRedirect [boolean] - Flag to make the request return a 200 instead of a 3xx status code
+     * @returns A string with the redirect URL if the user already consented to the specificied scope. Otherwise, an OAuthApp
+     * @see OAuthApp
+     */
+    public getInfoOrAuthorizationCodeInsideRedirectUrl = async (queryString: string, doNotRedirect?: boolean): Promise<string | OAuthApp> => {
+        if (!queryString.startsWith("?")) {
+            queryString = `?${queryString}`;
+        }
+        if (doNotRedirect) {
+            queryString += "&no_redirect=true";
+        }
+
+        const response = await this.axios.get<{ redirectUrl: string } | OAuthApp>(this.getEndpoint(`/v1/login/oauth/info${queryString}`), {
+            maxRedirects: 0,
+        });
+
+        if (response.status < 300) {
+            if ("redirectUrl" in response.data) {
+                return response.data.redirectUrl;
+            } else {
+                return response.data;
+            }
+        }
+
         return response.headers.location;
     };
 

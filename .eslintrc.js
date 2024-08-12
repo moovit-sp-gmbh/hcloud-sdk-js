35cc5ba8a0c926b28d304f1d94a10f03adc0a5cc
DariusWe <dariusweiberg@gmail.com>
2024-08-12 12:07:34 +0200

    chore: Change severity of eslint no-duplicate-enum-values rule to from 'error' to 'warning'

--- .eslintrc.js
+++ .eslintrc.js
@@ -52,6 +52,7 @@ module.exports = {
         "no-shadow-restricted-names": 2,
         "no-useless-catch": 1,
         "no-unused-vars": 0 /* should be 2, but atm corrupted. Workaround is the next line. More Info: https://stackoverflow.com/questions/68626276/node-must-be-provided-when-reporting-error-if-location-is-not-provided-error-wi */,
+        "@typescript-eslint/no-duplicate-enum-values": 1,
         "@typescript-eslint/no-unused-vars": [2, { argsIgnorePattern: "^_" }],
         "@typescript-eslint/no-this-alias": 0,
         "@typescript-eslint/no-empty-function": 1,

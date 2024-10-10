98c28c1d898ac0a44a7566b7e9e0c807774aad43
Jorge Brown <j.brown@moovit-sp.com>
2024-10-10 17:30:45 +0200

    docs: isStreamNodeSpecificationV1 comment

--- src/lib/interfaces/high5/wave/index.ts
+++ src/lib/interfaces/high5/wave/index.ts
@@ -105,6 +105,11 @@ export interface StreamNodeSpecificationV2 extends StreamNodeSpecification {
     path?: string;
     customNode?: StreamCustomNodeSpecification;
 }
+/**
+ * This functions will return true when the specVersion is undefined to account for older catalogs/nodes
+ * that existed before the specVersion was introduced. It is therefore assumed that any node without a specVersion
+ * has a spec that matches version 1.
+ */
 export function isStreamNodeSpecificationV1(s: StreamNodeSpecification): s is StreamNodeSpecificationV1 {
     return s.specVersion === undefined || s.specVersion === 1;
 }

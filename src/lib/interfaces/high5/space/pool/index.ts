aa8f99d9a31b7acb18da685fcc2c9189ec649138
Jorge Brown <j.brown@moovit-sp.com>
2024-11-11 17:41:33 +0100

    feat: add methods for agent pools CRUD operations

---
+++ src/lib/interfaces/high5/space/pool/index.ts
@@ -0,0 +1,25 @@
+import { ReducedSpace } from "../../../global";
+import { ReducedOrganization, ReducedUser } from "../../../idp";
+
+export type Pool = {
+    _id: string;
+    organization: ReducedOrganization;
+    space: ReducedSpace;
+    creator: ReducedUser;
+
+    name: string;
+    targets: string[];
+};
+
+export type PoolChange = Pool & { invalidTargets: { notMember: string[]; notExecutionTarget: string[] } };
+
+export type PoolTargetPatch = {
+    identifiers: string[];
+    operation: ArrayOperation;
+};
+
+export enum ArrayOperation {
+    ADD = "ADD",
+    REPLACE = "REPLACE",
+    REMOVE = "REMOVE",
+}

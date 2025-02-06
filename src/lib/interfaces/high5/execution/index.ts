1540900fab4b78cb651e99bf4e7b7a3a38b200ff
Jorge Brown <j.brown@moovit-sp.com>
2025-02-06 09:51:09 +0100

    feat: add REPLACE_NODE debug command

--- src/lib/interfaces/high5/execution/index.ts
+++ src/lib/interfaces/high5/execution/index.ts
@@ -1,10 +1,13 @@
-export type DebugCommand = ContinueDebugCommand | StepBackDebugCommand | StepForwardDebugCommand | SetValueDebugCommand;
+import { StreamNode } from "../space/execution";
+
+export type DebugCommand = ContinueDebugCommand | StepBackDebugCommand | StepForwardDebugCommand | SetValueDebugCommand | ReplaceNodeDebugCommand;
 
 export enum CommandType {
     CONTINUE = "CONTINUE",
     STEP_BACK = "STEP_BACK",
     STEP_FORWARD = "STEP_FORWARD",
     SET_VALUE = "SET_VALUE",
+    REPLACE_NODE = "REPLACE_NODE",
 }
 
 export type ContinueDebugCommand = {
@@ -21,4 +24,8 @@ export type SetValueDebugCommand = {
     uuid: string;
     key: string;
     value: unknown;
+};
+export type ReplaceNodeDebugCommand = {
+    type: CommandType.REPLACE_NODE;
+    node: StreamNode;
 };

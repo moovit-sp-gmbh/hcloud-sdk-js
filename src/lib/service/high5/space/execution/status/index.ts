793d4b9937a1c71e33df475b0a5689a6fee63904
DariusWe <dariusweiberg@gmail.com>
2025-02-13 11:51:00 +0100

    fix!: fix outdated getStreamExecutionStatus() method

--- src/lib/service/high5/space/execution/status/index.ts
+++ src/lib/service/high5/space/execution/status/index.ts
@@ -1,7 +1,5 @@
 import { AxiosInstance } from "axios";
 import Base, { Options } from "../../../../../Base";
-import { createPaginatedResponse } from "../../../../../helper/paginatedResponseHelper";
-import { PaginatedResponse } from "../../../../../interfaces/global";
 import { High5ExecutionStatus } from "../../../../../interfaces/high5/space/execution";
 
 export class High5SpaceExecutionStates extends Base {
@@ -10,24 +8,18 @@ export class High5SpaceExecutionStates extends Base {
     }
 
     /**
-     * Retrieves all stream execution logs for a given stream.
+     * Retrieves an execution status by ID.
      * @param orgName Name of the Organization
      * @param spaceName Name of the Space
-     * @param streamStatusId ID of the stream status
-     * @returns Object containing an array of Stream execution logs as well as the total number of results
+     * @param executionStatusId ID of the execution status
+     * @returns High5ExecutionStatus
      */
-    async getStreamExecutionStatus(
-        orgName: string,
-        spaceName: string,
-        streamStatusId: string,
-        limit = 25,
-        page = 0
-    ): Promise<PaginatedResponse<High5ExecutionStatus>> {
-        const resp = await this.axios.get<High5ExecutionStatus[]>(
-            this.getEndpoint(`/v1/org/${orgName}/spaces/${spaceName}/execution/streams/status/${streamStatusId}?page=${page}&limit=${limit}`)
+    async getStreamExecutionStatus(orgName: string, spaceName: string, executionStatusId: string): Promise<High5ExecutionStatus> {
+        const resp = await this.axios.get<High5ExecutionStatus>(
+            this.getEndpoint(`/v1/org/${orgName}/spaces/${spaceName}/execution/streams/status/${executionStatusId}`)
         );
 
-        return createPaginatedResponse(resp) as PaginatedResponse<High5ExecutionStatus>;
+        return resp.data;
     }
 
     protected getEndpoint(endpoint: string): string {

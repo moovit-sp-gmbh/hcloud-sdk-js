41649249104ac13dff2425811269d897e899df85
grisov <grisov.github@mailnull.com>
2025-03-19 17:31:13 +0100

    refactor: move part of parameter validation functions to core

--- src/lib/helper/ValidationHelper.ts
+++ src/lib/helper/ValidationHelper.ts
@@ -23,16 +23,10 @@ enum Entity {
 }
 
 interface Details {
-    name: string; // Name of the path parameter
-    description: string;
-    allowedCharacters?: string;
     pattern: RegExp;
-    showRegexp: boolean; // Determines whether to display the regexp pattern in the OpenAPI documentation
     symbols?: RegExp; // Allowed symbols, Normally they are determined automatically by 'pattern' but can be set manually in some cases
     minLength: number;
     maxLength: number;
-    example: string;
-    errorMessage?: string;
 }
 
 class EntityDetails {
@@ -42,22 +36,8 @@ class EntityDetails {
         this.entity = entityCollection[name];
     }
 
-    get name(): string {
-        return this.entity.name;
-    }
-
-    get description(): string {
-        const rules: string[] = this.entity.allowedCharacters ? [`only ${this.entity.allowedCharacters} are allowed`] : [];
-        rules.push(`it should contain between ${this.entity.minLength} and ${this.entity.maxLength} characters`);
-        return `${this.entity.description} (${rules.join("; ")})`;
-    }
-
     get pattern(): RegExp {
         return this.entity.pattern;
-    }
-
-    get showRegexp(): boolean {
-        return this.entity.showRegexp;
     }
 
     get minLength(): number {
@@ -66,18 +46,6 @@ class EntityDetails {
 
     get maxLength(): number {
         return this.entity.maxLength;
-    }
-
-    get example(): string {
-        return this.entity.example;
-    }
-
-    get errorMessage(): string {
-        const rules: string[] = this.entity.errorMessage ? [this.entity.errorMessage] : [];
-        if (this.entity.allowedCharacters) rules.push(`allowed only ${this.entity.allowedCharacters}`);
-        rules.push(`length should be between ${this.entity.minLength} and ${this.entity.maxLength}`);
-        rules.push(`pattern: ${this.entity.pattern.toString()}.`);
-        return `Invalid '${this.entity.name}' - ${rules.join(", ")}`;
     }
 
     /**
@@ -128,219 +96,115 @@ function ValidationHelper(name: Entity): EntityDetails {
 
 const entityCollection: Record<Entity, Details> = {
     [Entity.USER_NAME]: {
-        name: "name",
-        description: "Name of the user",
-        allowedCharacters: "alphanumeric characters, underscores, hyphens, dots and spaces",
         // Case-insensitive. Must contain at least 3 character. Allows letters, numbers, underscores, hyphens, dots, and spaces. Cannot start with a dash, end with a dash, or contain double dashes
         pattern: /^(?!-)(?!.*--)[\w-. ]{3,255}(?<!-)$/i,
-        showRegexp: true,
         minLength: 3,
         maxLength: 255,
-        example: "Helmut Mc Cloud",
     },
     [Entity.ORGANIZATION_NAME]: {
-        name: "orgName",
-        description: "Name of the organization",
-        allowedCharacters: "alphanumeric characters, hyphens and dots",
         // Case-insensitive. No spaces or other special characters allowed. Cannot start with a dash, end with a dash, or contain double dashes
         pattern: /^(?!-)(?!.*--)[a-zA-Z0-9-.]{3,64}(?<!-)$/i,
-        showRegexp: false,
         minLength: 3,
         maxLength: 64,
-        example: "MoovIT-Software-Products-GmbH",
     },
     [Entity.USER_ORGANIZATION_NAME]: {
-        name: "name",
-        description: "Name of the user's private organization, allowed everything defined in the RFC for the email specification",
         pattern: /^[\w-+@.]{3,255}$/i,
-        showRegexp: false,
         minLength: 3,
         maxLength: 255,
-        example: "hmc@helmut.cloud",
-        errorMessage: "it must follow everything defined in the RFC for the email specification",
     },
     [Entity.TEAM_NAME]: {
-        name: "teamName",
-        description: "Name of the team",
-        allowedCharacters: "alphanumeric characters, hyphens and dots",
         // Case-insensitive. No spaces or additional special characters allowed. Cannot start with a dash, end with a dash, or contain double dashes
         pattern: /^(?!-)(?!.*--)[a-zA-Z0-9-.]{3,64}(?<!-)$/i,
-        showRegexp: false,
         minLength: 3,
         maxLength: 64,
-        example: "Research-and-development-department.1",
     },
     [Entity.COMPANY_NAME]: {
-        name: "company",
-        description: "Name of the company the organization belongs to",
-        allowedCharacters: "alphanumeric characters, underscores, hyphens, dots and spaces",
         pattern: /^[\w-. ]{1,255}$/i,
-        showRegexp: false,
         minLength: 1,
         maxLength: 255,
-        example: "Moovit SP",
     },
     [Entity.PAT_NAME]: {
-        name: "name",
-        description: "Name of the Personal Access Token",
-        allowedCharacters: "alphanumeric characters, underscores, hyphens, dots and spaces",
         pattern: /^[\w-. ]{1,64}$/i,
-        showRegexp: false,
         minLength: 1,
         maxLength: 64,
-        example: "Private auth token",
     },
     [Entity.JOIN_TOKEN_NAME]: {
-        name: "name",
-        description: "Name of the join token",
-        allowedCharacters: "alphanumeric characters, underscores, hyphens and dots",
         pattern: /^[\w-.]{1,64}$/i,
-        showRegexp: false,
         minLength: 1,
         maxLength: 64,
-        example: "Join_token_for_Moovit-SP.",
     },
     [Entity.SPACE_NAME]: {
-        name: "spaceName",
-        description: "Name of the space",
-        allowedCharacters: "alphanumeric characters, hyphens and dots",
         pattern: /^[a-zA-Z0-9-.]{1,32}$/i,
-        showRegexp: false,
         minLength: 1,
         maxLength: 32,
-        example: "Helmut4",
     },
     [Entity.EVENT_NAME]: {
-        name: "eventName",
-        description: "Name of the event",
-        allowedCharacters: "alphanumeric characters, underscores, hyphens and dots",
         pattern: /^[\w-.]{1,128}$/i,
-        showRegexp: false,
         minLength: 1,
         maxLength: 128,
-        example: "Dev_Camp-2025.1",
     },
     [Entity.STREAM_NAME]: {
-        name: "name",
-        description: "Name of the stream, dots and spaces are allowed only inside a name",
-        allowedCharacters: "alphanumeric characters, underscores, hyphens, dots and spaces",
         pattern: /^(?![.\s]+)[\w-. ]{1,128}(?<![.\s]+)$/i,
-        showRegexp: false,
         symbols: /[\w-. ]/i,
         minLength: 1,
         maxLength: 128,
-        example: "Stream-bandwidth",
     },
     [Entity.DESIGN_NAME]: {
-        name: "name",
-        description: "Name of the design",
-        allowedCharacters: "alphabet characters, hyphens and underscores",
         pattern: /^[a-zA-Z-_]{1,64}$/i,
-        showRegexp: false,
         minLength: 1,
         maxLength: 64,
-        example: "Design-application",
     },
     [Entity.SNAPSHOT_NAME]: {
-        name: "name",
-        description: "Name of the snapshot",
-        allowedCharacters: "alphanumeric characters, underscores, hyphens, dots and spaces",
         pattern: /^[\w-. ]{1,64}$/i,
-        showRegexp: false,
         minLength: 1,
         maxLength: 64,
-        example: "This is version 1.0",
     },
     [Entity.WEBHOOK_NAME]: {
-        name: "name",
-        description: "Name of the webhook",
-        allowedCharacters: "alphanumeric characters, underscores, hyphens, dots and spaces",
         pattern: /^[\w-. ]{1,128}$/i,
-        showRegexp: false,
         minLength: 1,
         maxLength: 128,
-        example: "Webhook - green",
     },
     [Entity.SECRET_KEY]: {
-        name: "secretKey",
-        description: "Key of the secret",
-        allowedCharacters: "alphanumeric characters, underscores, hyphens and dots",
         pattern: /^[\w-.]{1,32}$/i,
-        showRegexp: false,
         minLength: 1,
         maxLength: 32,
-        example: "Helmut4-dev.api_key",
     },
     [Entity.SECRET_VALUE]: {
-        name: "value",
-        description: "Secret value",
         pattern: /^.{1,1024}$/i,
-        showRegexp: false,
         minLength: 1,
         maxLength: 1024,
-        example: "Something important!",
     },
     [Entity.POOL_NAME]: {
-        name: "poolName",
-        description: "Name of the pool",
-        allowedCharacters: "alphanumeric characters, underscores, hyphens and dots",
         pattern: /^[\w-.]{1,128}$/i,
-        showRegexp: false,
         minLength: 1,
         maxLength: 128,
-        example: "My-Pool",
     },
     [Entity.FUSE_JOB]: {
-        name: "name",
-        description: "Name of the cronjob",
         pattern: /^.{1,64}$/i,
-        showRegexp: false,
         minLength: 1,
         maxLength: 64,
-        example: "My cronjob",
     },
     [Entity.WAVE_ENGINE_RELEASE]: {
-        name: "version",
-        description: "Version of the release",
         pattern: /^\d+\.\d+\.\d+(-dev-\d+)?$/i,
-        showRegexp: false,
         symbols: /[dev0-9-.]/i,
         minLength: 1,
         maxLength: 128,
-        example: "1.3.0-dev-147",
-        errorMessage: "it must follow semantic version format, for example: '1.0.0' or '1.0.0-dev-1'",
     },
     [Entity.CHALLENGE_STRING]: {
-        name: "challenge",
-        description:
-            "Some third party services require to validate the target URL of a webhook first. To do so, they sent an initial GET request with a query-string called 'challenge'",
-        allowedCharacters: "alphanumeric characters, underscores and hyphens",
         pattern: /^[\w-]{1,128}$/i,
-        showRegexp: false,
         minLength: 1,
         maxLength: 128,
-        example: "Some-random-string-to-validate-the-target-URL-of-a-webhook",
     },
     [Entity.DATABASE_NAME]: {
-        name: "dbName",
-        description: "Name of the database",
-        allowedCharacters: "alphanumeric characters, underscores, hyphens and dots",
         pattern: /^[\w-.]{1,128}$/i,
-        showRegexp: false,
         minLength: 1,
         maxLength: 128,
-        example: "Helmut4-Storage",
     },
     [Entity.DOCUMENT_KEY]: {
-        name: "key",
-        description: "Key of the document",
-        allowedCharacters: "alphanumeric characters, underscores, hyphens and dots",
         pattern: /^[\w-.]{1,64}$/i,
-        showRegexp: false,
         minLength: 1,
         maxLength: 64,
-        example: "My_personal_web-page_address.1",
     },
 };
 
-export { Entity, ValidationHelper };
+export { Entity, EntityDetails, ValidationHelper };

0e4f7646a8a22d282ba6678b28e1b1721e87fceb
DariusWe <dariusweiberg@gmail.com>
2025-04-16 14:26:21 +0200

    feat: add endpoint to retrieve execution trees

--- src/lib/service/high5/space/execution/status/index.ts
+++ src/lib/service/high5/space/execution/status/index.ts
@@ -1,6 +1,6 @@
 import { AxiosInstance } from "axios";
 import Base, { Options } from "../../../../../Base";
-import { High5ExecutionStatus } from "../../../../../interfaces/high5/space/execution";
+import { ExecutionTree, High5ExecutionStatus } from "../../../../../interfaces/high5/space/execution";
 
 export class High5SpaceExecutionStates extends Base {
     constructor(options: Options, axios: AxiosInstance) {
@@ -17,6 +17,21 @@ export class High5SpaceExecutionStates extends Base {
     async getStreamExecutionStatus(orgName: string, spaceName: string, executionStatusId: string): Promise<High5ExecutionStatus> {
         const resp = await this.axios.get<High5ExecutionStatus>(
             this.getEndpoint(`/v1/org/${orgName}/spaces/${spaceName}/execution/streams/status/${executionStatusId}`)
+        );
+
+        return resp.data;
+    }
+
+    /**
+     * Retrieves the complete executionTree the stream execution belongs to. An executionTree represents the hierarchical structure of stream executions initiated by a 'Trigger Stream' node.
+     * @param orgName Name of the Organization
+     * @param spaceName Name of the Space
+     * @param executionStatusId ID of the execution status
+     * @returns ExecutionTree
+     */
+    async getExecutionTree(orgName: string, spaceName: string, executionStatusId: string): Promise<ExecutionTree> {
+        const resp = await this.axios.get<ExecutionTree>(
+            this.getEndpoint(`/v1/org/${orgName}/spaces/${spaceName}/execution/streams/executionTree/${executionStatusId}`)
         );
 
         return resp.data;

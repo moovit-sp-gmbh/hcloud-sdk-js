d68b218d0f1b1423b69adf1520192a3fa9e0b744
Yo Sev <severinsiebertz@googlemail.com>
2025-08-20 16:14:27 +0200

    fix: omit _id in createShare interface

--- src/lib/interfaces/cosmo/share/index.ts
+++ src/lib/interfaces/cosmo/share/index.ts
@@ -1,4 +1,4 @@
-import { ReducedUser } from "../../idp";
+import { ReducedUser } from "../../idp"
 
 export interface Share {
     _id: string;
@@ -8,7 +8,7 @@ export interface Share {
     items: Map<string, string[]>;
 }
 
-export interface ShareCreate extends Omit<Share, "id" | "createDate" | "creator"> {
+export interface ShareCreate extends Omit<Share, "_id" | "createDate" | "creator"> {
     password?: string;
     expires?: number;
     namespaces?: Map<string, string[]>;

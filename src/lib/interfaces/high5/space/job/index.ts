33c9c32c74d10ab141979fb417c0514e673e37fc
grisov <grisov.github@mailnull.com>
2025-11-24 21:12:21 +0100

    refactor!: update Cronjob interfaces to match High5 API

--- src/lib/interfaces/high5/space/job/index.ts
+++ src/lib/interfaces/high5/space/job/index.ts
@@ -1,7 +1,70 @@
-import { Select } from "../../../../utils/Types";
-import { Header, HttpMethod, ReducedSpace } from "../../../global";
+import { ReducedSpace } from "../../../global";
 import { ReducedOrganization } from "../../../idp/organization";
 import { ReducedUser } from "../../../idp/user";
+import { ReducedEvent } from "../event";
+
+enum IntervalType {
+    MINUTE = "minute",
+    HOUR = "hour",
+    DAY = "day",
+    MONTH = "month",
+    YEAR = "year",
+    CUSTOM = "custom",
+}
+
+interface MinuteCronjob {
+    type: IntervalType.MINUTE;
+    values: {
+        minute: number;
+    };
+}
+
+interface HourCronjob {
+    type: IntervalType.HOUR;
+    values: {
+        hour: number;
+    };
+}
+
+interface DayCronjob {
+    type: IntervalType.DAY;
+    values: {
+        hour: number;
+        minute: number;
+    };
+}
+
+interface MonthCronjob {
+    type: IntervalType.MONTH;
+    values: {
+        day: number;
+        hour: number;
+        minute: number;
+    };
+}
+
+interface YearCronjob {
+    type: IntervalType.YEAR;
+    values: {
+        month: number;
+        day: number;
+        hour: number;
+        minute: number;
+    };
+}
+
+interface CustomCronjob {
+    type: IntervalType.CUSTOM;
+    values: {
+        year: number;
+        month: number;
+        day: number;
+        hour: number;
+        minute: number;
+    };
+}
+
+type CronjobType = MinuteCronjob | HourCronjob | DayCronjob | MonthCronjob | YearCronjob | CustomCronjob;
 
 export interface Job {
     _id: string;
@@ -10,37 +73,19 @@ export interface Job {
     organization: ReducedOrganization;
     creator: ReducedUser;
     space: ReducedSpace;
-    targetUrl: string;
-    httpMethod: HttpMethod;
-    acceptInvalidSSL: boolean;
+    targetEvent: ReducedEvent;
     timezone: string;
     description?: string;
-    headers?: Header[];
-    body?: string;
+    payload?: string;
     enabled?: boolean;
     createDate: number;
     modifyDate: number;
     nextExecution?: number[];
     lastStatus?: number;
     lastTriggered?: number;
-    hmacSettings?: HmacSettings;
+    time?: CronjobType;
 }
 
-export type HmacSettings = {
-    headerName: string;
-    secret: string;
-    algorithm: HmacHashingAlgo;
+export type JobCreate = Pick<Job, "name" | "expression" | "payload" | "enabled" | "description" | "lastStatus" | "lastTriggered" | "time"> & {
+    targetEventId: string;
 };
-
-export enum HmacHashingAlgo {
-    SHA_1 = "SHA-1",
-    SHA_256 = "SHA-256",
-}
-
-export type JobCreate = Pick<
-    Job,
-    "name" | "expression" | "targetUrl" | "httpMethod" | "headers" | "body" | "enabled" | "description" | "lastStatus" | "lastTriggered"
-> &
-    Partial<Pick<Job, "acceptInvalidSSL" | "timezone">> & {
-        hmacSettings: Select<HmacSettings, "headerName" | "algorithm", "secret">;
-    };

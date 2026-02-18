0ff2f21c0993bfcc2ceec54e2c441a1652a19647
ArturGrafov <arturgrafov66@googlemail.com>
2026-02-18 14:42:15 +0100

    style: autoformat

--- src/lib/interfaces/high5/space/job/index.ts
+++ src/lib/interfaces/high5/space/job/index.ts
@@ -1,7 +1,7 @@
-import { ReducedSpace } from "../../../global"
-import { ReducedOrganization } from "../../../idp/organization"
-import { ReducedUser } from "../../../idp/user"
-import { ReducedEvent } from "../event"
+import { ReducedSpace } from "../../../global";
+import { ReducedOrganization } from "../../../idp/organization";
+import { ReducedUser } from "../../../idp/user";
+import { ReducedEvent } from "../event";
 
 enum IntervalType {
     MINUTE = "minute",

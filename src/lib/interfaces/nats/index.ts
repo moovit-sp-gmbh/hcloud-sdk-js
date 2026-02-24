3db614ca973ebadbdb28e1f488708c99bb8353fa
Yo Sev <severinsiebertz@googlemail.com>
2026-02-24 15:11:28 +0100

    feat: add optional parent to nats asset object

--- src/lib/interfaces/nats/index.ts
+++ src/lib/interfaces/nats/index.ts
@@ -1,7 +1,7 @@
-import { Msg } from "nats";
-import { Products } from "../global";
-import { High5ExecuteOnAgentRequest, High5ExecutionCancelRequest } from "../high5/space/execution";
-import { LicenseTier } from "../idp";
+import { Msg } from "nats"
+import { Products } from "../global"
+import { High5ExecuteOnAgentRequest, High5ExecutionCancelRequest } from "../high5/space/execution"
+import { LicenseTier } from "../idp"
 
 enum NatsSubject {
     IDP_USER_GENERAL = "hcloud.idp.user.${userId}.general",
@@ -230,6 +230,11 @@ interface NatsIdNoUnderscoreObject {
 
 interface NatsAssetObject {
     _id: string;
+    parent?: {
+        _id: string;
+        name: string;
+        type: "ASSET" | "SPACE" | "PRODUCTION" | "PROJECT" | "FOLDER";
+    };
     type: "ASSET" | "SPACE" | "PRODUCTION" | "PROJECT" | "FOLDER";
 }
 
@@ -632,4 +637,5 @@ const base64Encode = (str: string) => {
     }
 };
 
-export { NatsCallback, NatsMessage, NatsMessageType, NatsObject, NatsObjectType, NatsSubject, NatsSubjects, Msg as RawMsg };
+export { NatsCallback, NatsMessage, NatsMessageType, NatsObject, NatsObjectType, NatsSubject, NatsSubjects, Msg as RawMsg }
+

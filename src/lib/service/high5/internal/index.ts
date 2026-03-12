82c0d0b1d7415220c6151afd6dca80944da5f065
grisov <grisov.github@mailnull.com>
2026-03-12 15:53:53 +0100

    feat(high5): internal endpoint to delete all documents of organization

--- src/lib/service/high5/internal/index.ts
+++ src/lib/service/high5/internal/index.ts
@@ -20,6 +20,11 @@ export class High5Internal extends Base {
         return (raw?.raw ? resp : undefined) as MaybeRaw<R, void>;
     }
 
+    async deleteDocumentsOfOrganization<R extends boolean = false>(orgId: string, raw?: { raw: R }): Promise<MaybeRaw<R, void>> {
+        const resp = await this.axios.delete(this.getEndpoint(`/v1/org/${orgId}/documents`));
+        return (raw?.raw ? resp : undefined) as MaybeRaw<R, void>;
+    }
+
     protected getEndpoint(endpoint: string): string {
         return `${this.options.server}/api/high5/internal${endpoint}`;
     }

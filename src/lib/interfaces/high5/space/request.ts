d0d5dae9bf2ebef6b97eeef76af1353881e8c951
grisov <grisov.github@mailnull.com>
2026-04-27 15:15:12 +0200

    feat(high5): add capturedRequest method

--- src/lib/interfaces/high5/space/request.ts
+++ src/lib/interfaces/high5/space/request.ts
@@ -2,12 +2,10 @@ import { IncomingHttpHeaders } from "http";
 import { HttpMethod } from "../../global";
 
 export interface CapturedRequest {
-    _id: string;
     method: HttpMethod;
     headers: IncomingHttpHeaders;
     body: any;
     spaceId: string;
     organizationId: string;
     creatorId: string;
-    createDate: number;
 }

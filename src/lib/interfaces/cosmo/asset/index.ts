1c8ac6cd9aabd4fc19bf1f457f09cd5d6c67a443
YoSev <severinsiebertz@googlemail.com>
2026-05-12 14:46:01 +0200

    feat(cosmo): add purgeDate to BaseAsset

--- src/lib/interfaces/cosmo/asset/index.ts
+++ src/lib/interfaces/cosmo/asset/index.ts
@@ -17,6 +17,7 @@ export interface BaseAsset {
     space: ReducedSpace;
     permissions?: string[];
     breadcrumb?: (Asset | CosmoSpace)[];
+    purgeDate?: number;
 }
 
 export type Asset = BaseAsset &

800c43e66843c6ab0cd13bb4364ba249f94f9596
grisov <grisov.github@mailnull.com>
2026-06-10 17:55:18 +0200

    feat(ci): move release trigger to main
    Move release trigger to main and publish to npmjs and GitHub Packages simultaneously

--- release.config.js
+++ release.config.js
@@ -2,7 +2,7 @@
  * @type {import('semantic-release').GlobalConfig}
  */
 module.exports = {
-    branches: ["main", { name: "staging", prerelease: true }],
+    branches: ["main"],
     plugins: [
         [
             "@semantic-release/commit-analyzer",

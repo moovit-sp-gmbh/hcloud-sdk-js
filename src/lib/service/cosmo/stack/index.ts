fcdf27052471889004c259b487038331de35e67d
ArturGrafov <arturgrafov66@googlemail.com>
2026-07-13 16:06:54 +0200

    fix: add/remove assets to/from stack; ci: update node version reset pnpm version

--- src/lib/service/cosmo/stack/index.ts
+++ src/lib/service/cosmo/stack/index.ts
@@ -23,25 +23,25 @@ export class CosmoStack extends Base {
     }
 
     /**
-     * Attach an Asset to an existing Stack in Cosmo.
+     * Attach Assets to an existing Stack in Cosmo.
      *
      * @param organizationName Name of the Organization
      * @param spaceName Name of the Space
      * @param stackId ID of the Stack
-     * @param assetId ID of the Asset to attach
+     * @param assetIds Array of Asset IDs to attach
      * @param raw (optional) If true, returns the raw Axios response
      * @returns The updated Stack
      */
-    async attachAssetToStack<R extends boolean = false>(
+    async attachAssetsToStack<R extends boolean = false>(
         organizationName: string,
         spaceName: string,
         stackId: string,
-        assetId: string,
+        assetIds: string[],
         raw?: { raw: R }
     ): Promise<MaybeRaw<R, Stack>> {
-        const resp = await this.axios.post<Stack>(
-            this.getEndpoint(`/v1/org/${organizationName}/spaces/${spaceName}/stacks/${stackId}/assets/${assetId}`)
-        );
+        const resp = await this.axios.post<Stack>(this.getEndpoint(`/v1/org/${organizationName}/spaces/${spaceName}/stacks/${stackId}/assets/add`), {
+            assetIds: assetIds,
+        });
 
         return (raw?.raw ? resp : resp.data) as MaybeRaw<R, Stack>;
     }
@@ -52,20 +52,20 @@ export class CosmoStack extends Base {
      * @param organizationName Name of the Organization
      * @param spaceName Name of the Space
      * @param stackId ID of the Stack
-     * @param assetId ID of the Asset to detach
+     * @param assetIds Array of Asset IDs to detach
      * @param raw (optional) If true, returns the raw Axios response
      * @returns The updated Stack
      */
-    async detachAssetFromStack<R extends boolean = false>(
+    async detachAssetsFromStack<R extends boolean = false>(
         organizationName: string,
         spaceName: string,
         stackId: string,
-        assetId: string,
+        assetIds: string[],
         raw?: { raw: R }
     ): Promise<MaybeRaw<R, Stack>> {
-        const resp = await this.axios.delete<Stack>(
-            this.getEndpoint(`/v1/org/${organizationName}/spaces/${spaceName}/stacks/${stackId}/assets/${assetId}`)
-        );
+        const resp = await this.axios.post<Stack>(this.getEndpoint(`/v1/org/${organizationName}/spaces/${spaceName}/stacks/${stackId}/assets/remove`), {
+            assetIds: assetIds,
+        });
 
         return (raw?.raw ? resp : resp.data) as MaybeRaw<R, Stack>;
     }

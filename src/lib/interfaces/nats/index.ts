878dbedc43a3ea5cae86c4a0f6b0c68e43f19268
ArturGrafov <arturgrafov66@googlemail.com>
2026-09-08 13:54:57 +0200

    revert: introduce a new nats subject for standard user information
    This reverts commit 9236d8075ae6bf4e2107d6c942da6c568503fa65.

--- src/lib/interfaces/nats/index.ts
+++ src/lib/interfaces/nats/index.ts
@@ -6,7 +6,6 @@ import { CapturedRequest } from "../high5/space/request";
 import { LicenseTier } from "../idp";
 
 enum NatsSubject {
-    IDP_USER = "hcloud.idp.user.${userId}",
     IDP_USER_GENERAL = "hcloud.idp.user.${userId}.general",
     IDP_USER_PROFILE = "hcloud.idp.user.${userId}.profile",
     IDP_USER_NOTIFICATIONS = "hcloud.idp.user.${userId}.notifications",
@@ -83,7 +82,6 @@ enum NatsSubject {
 type NatsSubjectReplacements = {
     namespaceName?: string;
     userId?: string;
-    msg?: string;
     organizationName?: string;
     organizationId?: string;
     spaceName?: string;
@@ -183,7 +181,6 @@ interface NatsObject
     extends
         NatsNameObject,
         NatsIdObject,
-        NatsUserInformObject,
         NatsMemberObject,
         NatsSecretObject,
         NatsExecTargetObject,
@@ -198,7 +195,6 @@ interface NatsObject
         NatsAgentConnectionObject,
         NatsCosmoStatusObject {
     [NatsSubject.IDP_USER_GENERAL]: NatsIdObject;
-    [NatsSubject.IDP_USER]: NatsUserInformObject;
     [NatsSubject.IDP_USER_PROFILE]: NatsIdObject;
     [NatsSubject.IDP_USER_SECURITY_PATS]: NatsIdObject;
     [NatsSubject.IDP_USER_SECURITY_GENERAL]: unknown;
@@ -258,10 +254,6 @@ interface NatsIdObject {
 }
 interface NatsIdNoUnderscoreObject {
     id: string;
-}
-
-interface NatsUserInformObject extends NatsIdObject {
-    msg: string;
 }
 
 interface NatsAssetObject {
@@ -334,9 +326,6 @@ type NatsCallback = (err: Error | null, msg?: NatsMessage, rawMsg?: Msg) => void
  */
 class NatsSubjects {
     static IDP = class {
-        static USER = (userId: string, msg: string) => {
-            return NatsSubjects.replace(NatsSubject.IDP_USER, { userId, msg });
-        };
         static User = class {
             static GENERAL = (userId: string) => {
                 return NatsSubjects.replace(NatsSubject.IDP_USER_GENERAL, { userId });
@@ -716,7 +705,6 @@ class NatsSubjects {
         );
         subject = subject.replace("${watchFolderId}", replacements.watchFolderId || "null");
         subject = subject.replace("${agentUuid}", replacements.agentUuid || "null");
-        subject = subject.replace("${msg}", replacements.msg || "null");
 
         return subject;
     };

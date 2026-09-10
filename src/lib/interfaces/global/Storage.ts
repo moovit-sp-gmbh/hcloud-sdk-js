45a8850a3d8889f7742e0dc0caec2e476183069d
ArturGrafov <arturgrafov66@googlemail.com>
2026-09-10 13:50:41 +0200

    fix: add missing endpoints; add new spaces property

--- src/lib/interfaces/global/Storage.ts
+++ src/lib/interfaces/global/Storage.ts
@@ -1,4 +1,5 @@
 import { ReducedOrganization, ReducedUser } from "../idp";
+import { ReducedSpace } from "./Space";
 
 export type Storage = {
     _id: string;
@@ -15,6 +16,8 @@ export type Storage = {
     region: string;
     accessKeyId: string;
     secretAccessKey: string;
+
+    spaces: ReducedSpace[];
 
     valid: boolean;
     errorMessage?: string;
@@ -33,8 +36,6 @@ export type StorageConfiguration = {
 };
 
 export type StorageCreateDto = Pick<Storage, "name" | "endpoint" | "bucket" | "region"> & { accessKeyId: string; secretAccessKey: string };
-export type StoragePatchDto = Partial<
-    Pick<Storage, "name" | "endpoint" | "bucket" | "region"> & { accessKeyId: string; secretAccessKey: string }
->;
+export type StoragePatchDto = Partial<Pick<Storage, "name" | "endpoint" | "bucket" | "region"> & { accessKeyId: string; secretAccessKey: string }>;
 
 export type ReducedStorage = Pick<Storage, "_id" | "name" | "isDefault">;
